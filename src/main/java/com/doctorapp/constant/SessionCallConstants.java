52b916d46dc277a146548b8c0056d200dd441aff
🐱  Bingyu Wang <wanbingy@amazon.com>
2021-05-19 10:34:05 -0700

    Update client id

--- src/main/java/com/doctorapp/constant/SessionCallConstants.java
+++ src/main/java/com/doctorapp/constant/SessionCallConstants.java
@@ -2,7 +2,7 @@ package com.doctorapp.constant;
 
 public class SessionCallConstants {
 
-    public static final String TOKEN_URL = "https://telehealth.lucuncai.com/oauth/token?grant_type=client_credentials&client_id=wanbingy_telehealth_client&client_secret=password";
+    public static final String TOKEN_URL = "https://telehealth.lucuncai.com/oauth/token?grant_type=client_credentials&client_id=wanbingy_telehealth_client_custom&client_secret=password";
 
     public static final String LWA_TOKEN_URL = "https://telehealth.lucuncai.com/api/partner/token?partner_id=wanbingy_telehealth_client&user_id=%s&access_token=%s";
 
